c1f9516da4fd2b9593288241c277f2e30d7e3875
José Pedro <jgomes@geobit.com.br>
2021-09-23 21:00:10 -0300

    feat: adicionando modelo do cnab

--- app/cnab/models.py
+++ app/cnab/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from app.cnab.choices import TIPO_TRANSACAO
+
+
+class Cnab(models.Model):
+    tipo = models.CharField("Tipo de Transação", choices=TIPO_TRANSACAO)
+    data = models.DateField("Data da ocorrência")
+
+    valor = models.DecimalField(
+        "Valor de Movimentação", 
+        decimal_places=2, 
+        max_digits=10
+    )
+    cpf = models.CharField("CPF do Beneficiário", max_length=11)
+    cartao = models.CharField("Cartão", max_length=11)
+    hora = models.CharField("Hora (UTC-3)", max_length=6)
+    dono_loja = models.CharField("Dono da Loja", max_length=14)
+    nome_loja = models.CharField("Nome da Loja", max_length=19)
