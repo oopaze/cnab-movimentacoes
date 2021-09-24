69728de7c7e4b4d8e9844a51d12fbbed9747d240
José Pedro <jgomes@geobit.com.br>
2021-09-23 22:04:52 -0300

    feat: adicionando classe responsavel por ler arquivo com movimentacoes

---
+++ app/movimentacao/handlers/movimentacoes_reader.py
@@ -0,0 +1,40 @@
+import pytz
+
+from django.utils.timezone import datetime
+from decimal import Decimal
+
+
+class MovimentacoesFileHandle(object):
+    def __init__(self, file):
+        self.file = file
+        self.raw_file = file.read().decode()
+
+    def handle_movimentacoes(self):
+        movimentacao_list = []
+        movimentacoes_lines = self.raw_file.split('\n')
+
+        for movimentacao in movimentacoes_lines:
+            tipo = int(movimentacao[0])
+            data = datetime.strptime(
+                movimentacao[1:9] + movimentacao[42:48], "%Y%m%d%H%M%S")
+
+            valor = Decimal(movimentacao[9:19]) / 100
+            dono = self.retira_espacos_em_branco(movimentacao[48:62])
+            nome_loja = self.retira_espacos_em_branco(movimentacao[62:])
+
+            movimentacao_list.append({
+                "tipo": tipo,
+                "data": data,
+                "valor": valor,
+                "cpf": movimentacao[20:30],
+                "cartao": movimentacao[30:42],
+                "dono_loja": dono,
+                "nome_loja": nome_loja,
+            })
+
+        return movimentacao_list
+
+    def retira_espacos_em_branco(self, value):
+        return value.rstrip(" ").lstrip(" ")
+
+        
