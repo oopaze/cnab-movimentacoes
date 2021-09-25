ff1c5e7c9a2b1615a0e213af48b0ff852673f802
José Pedro <jgomes@geobit.com.br>
2021-09-25 06:00:52 -0300

    feat: adicionando template da view de list junto a seus arquivos estaticos

--- app/src/settings.py
+++ app/src/settings.py
@@ -125,4 +125,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_URL = '/static/'
---
+++ app/static/js/movimentacao.js
@@ -0,0 +1,51 @@
+function activeItem(id, data){
+    var items = document.querySelectorAll(".item")  
+    items.forEach((item) => { 
+        if (item.getAttribute("item-data") === id){
+            item.classList.add("active") 
+            writeData(data)
+        } else {
+            item.classList.remove("active") 
+        }
+    })
+}
+
+function writeData(data){
+    document.querySelector(".item-foco .item #numero").innerText = data.object.cartao
+    document.querySelector(".item-foco .item #valor").innerText = `R$ ${data.object.valor}`
+    document.querySelector(".item-foco .item #cpf").innerText = data.object.cpf
+    document.querySelector(".item-foco .item #destinatario").innerText = data.object.dono_loja
+    document.querySelector(".item-foco .item #local-compra").innerText = data.object.nome_loja
+    document.querySelector(".item-foco .item #data-compra").innerText = data.object.data
+    document.querySelector(".item-foco .item #natureza").innerText = data.object.natureza
+    document.querySelector(".item-foco .item #saidas_value").innerText = `R$ ${data.object.pago}`
+    document.querySelector(".item-foco .item #entradas_value").innerText = `R$ ${data.object.recebido}`
+    document.querySelector(".item-foco .item #saldo").innerText = `R$ ${data.object.saldo}`
+
+    if (data.object.natureza === 'saida'){
+        document.querySelector("#label_valor").innerText = "Valor Pago"
+    } else {
+        document.querySelector("#label_valor").innerText = "Valor Recebido"
+    }
+}
+
+async function getMoreDetails(id){
+    return await fetch(`/movimentacao/${id}/`, {
+        method: 'GET'
+    }).then(
+        (response) => response.json()
+    ).then((data) => {
+        return data
+    })
+}
+
+const items = document.querySelectorAll(".item")
+items.forEach(function(item) {
+    let id = item.getAttribute("item-data")
+    item.addEventListener("click", async function(e) { 
+        let data = await getMoreDetails(id)
+        if(data.success){
+            activeItem(id, data)
+        }
+    })
+})
