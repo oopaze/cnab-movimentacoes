f3420a592657867fca8d48fbd303cf3a8f90cabc
José Pedro <jgomes@geobit.com.br>
2021-09-28 19:23:16 -0300

    feat: adicionando configurações do docker

--- app/src/settings.py
+++ app/src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY", default="uma chave bem segura")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
